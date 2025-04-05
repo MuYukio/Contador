ef233ab5095f7cd8b616f1e3ad231277d837c894
MuYukio <muyukiom@gmail.com>
2025-04-04 21:11:06 -0400

    Finalização do aplicativo de contador

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { Text, TextInput, TouchableOpacity, View,StyleSheet, ScrollView } from "react-native";
 
+
+//Componente principal da aplicação
 export default function Index() {
 
   const[contador,setContador] = useState(0);
   const[verifica,setVerifica] = useState('Par');
   const[number, setNumber] = useState(1);
   const[historico,setHistorico] = useState<Number[]>([])
+  const[autoContar,setautoContar]= useState<boolean>(false);
 
+  function getcolor(){
+    if(contador > 0) return 'green';
+    if(contador < 0) return 'red';
+    return 'gray'
+  }
+
+  //adiciona um valor ao contador
   function adicionarContador(){
     setContador(number + contador )
     adicionaHistorico()
   }
+  //diminui um valor ao contador
   function diminuirContador(){
     setContador(contador - number )
     adicionaHistorico()
 
   }
+  //limpa todo os estados e o array do historico
   function zerarContador(){
     setContador(0)
-   setHistorico([])
-
+    setHistorico([])
+    setNumber(1)
+    setautoContar(false)//altera o botao de autocontagem
   }
   function ehVazio(item: number){
     if(item > 0){
@@ -34,26 +47,43 @@ export default function Index() {
     
   }
 
+  //altera o valor do booleano do auto contado
   function adicionaHistorico(){
     if(contador !== null){
       setHistorico( oldState => [...oldState, contador])
     }
-   
-  
   }
 
+  function toggleAutoContar(){
+    setautoContar((prev) => (!prev))
+  }
+
+  useEffect(() => {
+    let intervalId: ReturnType<typeof setInterval>
+
+    if(autoContar){
+        intervalId = setInterval(() => {
+          adicionarContador()
+        }, 1000)
+    }
+    return () => {
+      if(intervalId){
+        clearInterval(intervalId)
+      }
+    }
+  }, [autoContar, contador, number])
 
   return (
     <View
     style ={styles.container}
     >
       <Text
-      style ={styles.title}
+        style ={styles.title}
       >
         Contador Inteligente
       </Text>
       
-      <Text>
+      <Text style ={[styles.numero, {color: getcolor()}]}>
         {contador}
       </Text>
       <Text>
@@ -90,7 +120,6 @@ export default function Index() {
         value={String(number)}
         keyboardType="numeric"
         onChangeText={(item) => ehVazio(parseInt(item))}
-               
       />
       <View
       style = {styles.deLadinho}>
@@ -107,11 +136,10 @@ export default function Index() {
         <TouchableOpacity
           activeOpacity={0.783}
           style  = {styles.button}
-          //onPress={}
+          onPress={toggleAutoContar}
         >
-          <Text >
-            Iniciar auto
-          </Text>
+          <Text>{autoContar ? 'Parar auto': 'Iniciar auto'}</Text>
+
         </TouchableOpacity>
       </View>
       <Text>
@@ -164,7 +192,11 @@ const styles = StyleSheet.create({
     alignItems:'center',
    
   },
-  
+  numero:{
+    fontSize:48,
+    marginBottom:20,
+
+  }
 
 })
 
